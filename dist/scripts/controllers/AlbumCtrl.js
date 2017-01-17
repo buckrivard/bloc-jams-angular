3e47e1e9e67ffe732d98c7bf6d133ca0a0da6b3f
Buck Rivard <buckrivard@gmail.com>
2017-01-16 18:09:42 -0800

    Fixtures service added

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,13 +1,10 @@
 (function() {
-    function AlbumCtrl() {
-        this.albumData = albumPicasso;
-        console.log(this.albumData)
-        //for (var i = 0; i < albumData.songs.length; i++) {
-        //    this.albumData.push(angular.copy(albumPicasso));
+    function AlbumCtrl(Fixtures) {
+        this.albumData = Fixtures.getAlbum();
     }
     
     
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl);
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
