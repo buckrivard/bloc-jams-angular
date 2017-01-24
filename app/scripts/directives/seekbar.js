1433de98ae122700caccdde90bf888d1e94a35fe
Buck Rivard <buckrivard@gmail.com>
2017-01-23 16:11:17 -0800

    seekbar functionality added

--- app/scripts/directives/seekbar.js
+++ app/scripts/directives/seekbar.js
@@ -1,13 +1,59 @@
 (function() {
-	function seekBar() {
+	function seekBar($document) {
 		return {
 			templateUrl: '/templates/directives/seek_bar.html',
 			replace: true,
-			restrict: 'E'
+			restrict: 'E',
+			scope: {},
+			link: function (scope, element, attribute) {
+				scope.value = 0;
+				scope.max = 100;
+
+				var calculatePercent = function (seekBar, event) {
+					var offsetX = event.pageX - seekBar.offset().left;
+					var seekBarWidth = seekBar.width();
+					var offsetXPercent = offsetX / seekBarWidth;
+					offsetXPercent = Math.max(0, offsetXPercent);
+					offsetXPercent = Math.min(1, offsetXPercent);
+					return offsetXPercent;
+				}
+
+				var percentString = function() {
+					var value = scope.value;
+					var max = scope.max;
+					var percent = value / max * 100;
+					return percent + "%";
+				}
+
+				var seekBar = $(element);
+
+				scope.fillStyle = function () {
+					return {width: percentString()};
+				};
+
+				scope.onClickSeekBar = function (event) {
+					var percent = calculatePercent(seekBar, event);
+					scope.value = percent * scope.max;
+				};
+
+				scope.trackThumb = function() {
+					$document.bind('mousemove.thumb', function (event) {
+						var percent = calculatePercent(seekBar, event);
+						scope.$apply(function() {
+							scope.value = percent * scope.max;
+						});
+					});
+
+					$document.bind('mouseup.thumb', function (event) {
+						$document.unbind('mousemove.thumb');
+						$document.unbind('mouseup.thumb');
+					});
+				}
+			}
 		};
 	}
 
 	angular
 		.module('blocJams')
-		.directive('seekBar', seekBar)
+		.directive('seekBar', ['$document', seekBar])
 })();
